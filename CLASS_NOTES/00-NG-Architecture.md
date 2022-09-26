8ea49620397a6824ef4b16805e90fc93ee2030a2
Jeff Gonzalez <Jeff@Hypertheory.com>
2022-09-26 15:05:17 -0400

    before monday afternoon break

--- CLASS_NOTES/00-NG-Architecture.md
+++ CLASS_NOTES/00-NG-Architecture.md
@@ -1,1 +1,9 @@
 # Angular Architecture
+
+Angular is for building application. It is not a good tool for building websites.
+
+You have a an HTML page that is delivered to the user.
+
+The HTML page has an element in it that will contain our running angular application.
+
+That is created by downloading a bunch of javascript and that javascript "Creates" the application right in front of the users very eyes.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandingComponent } from './components/branding/branding.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BrandingComponent, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

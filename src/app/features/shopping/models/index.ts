2c0cc0b4049efa1adfea3ced9bb3eb74623b0817
Jeff Gonzalez <Jeff@Hypertheory.com>
2022-09-27 11:30:56 -0400

    before break

--- src/app/features/shopping/models/index.ts
+++ src/app/features/shopping/models/index.ts
@@ -2,3 +2,9 @@ export type ShoppingListItem = {
   description: string;
   purchased: boolean;
 };
+
+export type ShoppingSummaryModel = {
+  total: number;
+  purchased: number;
+  unpurchased: number;
+};
